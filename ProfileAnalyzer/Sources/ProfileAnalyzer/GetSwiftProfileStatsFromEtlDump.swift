a5f9b9423908cfba0f572e38a37f5c43a69321fd
Sameer Sundresh <sameer@sundresh.org>
2025-12-10 18:39:24 -0700

    Add --time-weighted mode to get-swift-profile-stats-from-etl-dump

--- ProfileAnalyzer/Sources/ProfileAnalyzer/GetSwiftProfileStatsFromEtlDump.swift
+++ ProfileAnalyzer/Sources/ProfileAnalyzer/GetSwiftProfileStatsFromEtlDump.swift
@@ -5,24 +5,40 @@ enum ETLFileError: Error {
     case traceStartTicksNotFound
 }
 
-struct ProfileSample: Decodable {
+struct ProfileSample {
     var timeOffset: Int64
     var program: String
+    var processId: Int
+    var threadId: Int
     var stack: [String]
     var numSamples: Int
 }
 
-struct ProgramAndFunction: Decodable, Hashable {
+struct ProgramAndFunction: Hashable {
     let program: String
     let function: String
 }
 
-struct ProgramAndFunctionInfo {
+struct ProcessIdAndThreadId: Hashable {
+    let processId: Int
+    let threadId: Int
+}
+
+struct NumSamplesAndCumulativeTime {
     var numSamples: Int = 0
-    var calledFunctionToNumnSamples: [String: Int] = [:]
+    var cumulativeTime: Int64 = 0
+}
+
+struct ProgramAndFunctionInfo {
+    var numSamplesAndCumulativeTime: NumSamplesAndCumulativeTime = NumSamplesAndCumulativeTime()
+    var calledFunctionToNumSamplesAndCumulativeTime: [String: NumSamplesAndCumulativeTime] = [:]
 }
 
 struct GetSwiftProfileStatsFromETLDump: ParsableCommand {
+    enum CommandLineArgumentsError: Error {
+        case tooManyWeightFlags
+    }
+
     @Argument(help: "Path to the ETL dump text file.")
     var etlDumpFilePath: String
 
@@ -32,18 +48,39 @@ struct GetSwiftProfileStatsFromETLDump: ParsableCommand {
     @Option(help: "Do not show functions that spend at least a certain percentage of their time directly calling a single other function")
     var pruneThreshold: Double = 99
 
+    @Flag var sampleFrequencyWeighted = false
+    @Flag var timeWeighted = false
+
+    mutating func validate() throws {
+        switch [sampleFrequencyWeighted, timeWeighted].count(where: { $0 }) {
+            case 0: sampleFrequencyWeighted = true
+            case 1: break
+            case _: throw CommandLineArgumentsError.tooManyWeightFlags
+        }
+    }
+
     func run() throws {
         let swiftEtlDump = try SwiftProfileETLDump(etlDumpFilePath: etlDumpFilePath)
         var programAndFunctionToInfo: [ProgramAndFunction: ProgramAndFunctionInfo] = [:]
         var numProgramSamplesSeen = 0
+        var processIdAndThreadIdToLastTimeOffset: [ProcessIdAndThreadId: Int64] = [:]
         for profileSample in try swiftEtlDump.getProfileSamples() {
-            let programName = profileSample.program.split(separator: " ", maxSplits: 1).first.map(String.init) ?? profileSample.program
+            let processIdAndThreadId = ProcessIdAndThreadId(processId: profileSample.processId, threadId: profileSample.threadId)
+            let timeSinceLastSampleInThisThread = profileSample.timeOffset - (processIdAndThreadIdToLastTimeOffset[processIdAndThreadId] ?? (profileSample.timeOffset - 1))
+            processIdAndThreadIdToLastTimeOffset[processIdAndThreadId] = profileSample.timeOffset
             for (i, function) in Set(profileSample.stack).enumerated() {
-                let programAndFunction = ProgramAndFunction(program: programName, function: function)
-                programAndFunctionToInfo[programAndFunction, default: ProgramAndFunctionInfo()].numSamples += profileSample.numSamples
+                let programAndFunction = ProgramAndFunction(program: profileSample.program, function: function)
+                var programAndFunctionInfo = programAndFunctionToInfo[programAndFunction, default: ProgramAndFunctionInfo()]
+                programAndFunctionInfo.numSamplesAndCumulativeTime.numSamples += profileSample.numSamples
+                programAndFunctionInfo.numSamplesAndCumulativeTime.cumulativeTime += timeSinceLastSampleInThisThread
                 if i > 0 {
-                    programAndFunctionToInfo[programAndFunction].calledFunctionToNumnSamples[profileSample.stack[i-1], default: 0] += profileSample.numSamples
+                    let calledFunction = profileSample.stack[i-1]
+                    var numSamplesAndCumulativeTime = programAndFunctionInfo.calledFunctionToNumSamplesAndCumulativeTime[calledFunction, default: NumSamplesAndCumulativeTime()]
+                    numSamplesAndCumulativeTime.numSamples += profileSample.numSamples
+                    numSamplesAndCumulativeTime.cumulativeTime += timeSinceLastSampleInThisThread
+                    programAndFunctionInfo.calledFunctionToNumSamplesAndCumulativeTime[calledFunction] = numSamplesAndCumulativeTime
                 }
+                programAndFunctionToInfo[programAndFunction] = programAndFunctionInfo
             }
             numProgramSamplesSeen += 1
             if numProgramSamplesSeen % 100 == 0 {
@@ -52,12 +89,27 @@ struct GetSwiftProfileStatsFromETLDump: ParsableCommand {
         }
         print("")
         print("")
-        print("program,function,frequency")
-        for element in programAndFunctionToNumSamples.sorted(by: { $0.value > $1.value })[..<topN] {
-            // Skip if there is a single directly called function that appears in >{pruneThreshold}% of the function's samples.
-            if programAndFunctionToCalledFunctionsToNumSamples[element.key]?.values.max() ?? 0 < element.value - Int(Double(element.value) * (1 - pruneThreshold/100)) {
-                let frequency = Double(element.value) / Double(numProgramSamplesSeen)
-                print("\(csvEscape(element.key.program)),\(csvEscape(element.key.function)),\(frequency)")
+        if sampleFrequencyWeighted {
+            print("program,function,frequency")
+            for element in programAndFunctionToInfo.sorted(by: { $0.value.numSamplesAndCumulativeTime.numSamples > $1.value.numSamplesAndCumulativeTime.numSamples })[..<topN] {
+                let programAndFunctionInfo = element.value
+                let numSamples = programAndFunctionInfo.numSamplesAndCumulativeTime.numSamples
+                // Skip if there is a single directly called function that appears in >{pruneThreshold}% of the function's samples.
+                if programAndFunctionInfo.calledFunctionToNumSamplesAndCumulativeTime.values.map(\.numSamples).max() ?? 0 < numSamples - Int(Double(numSamples) * (1 - pruneThreshold/100)) {
+                    let frequency = Double(numSamples) / Double(numProgramSamplesSeen)
+                    print("\(csvEscape(element.key.program)),\(csvEscape(element.key.function)),\(frequency)")
+                }
+            }
+        } else if timeWeighted {
+            print("program,function,seconds")
+            for element in programAndFunctionToInfo.sorted(by: { $0.value.numSamplesAndCumulativeTime.cumulativeTime > $1.value.numSamplesAndCumulativeTime.cumulativeTime })[..<topN] {
+                let programAndFunctionInfo = element.value
+                let cumulativeTime = programAndFunctionInfo.numSamplesAndCumulativeTime.cumulativeTime
+                // Skip if there is a single directly called function that takes up >{pruneThreshold}% of the function's time.
+                if programAndFunctionInfo.calledFunctionToNumSamplesAndCumulativeTime.values.map(\.cumulativeTime).max() ?? 0 < cumulativeTime - Int64(Double(cumulativeTime) * (1 - pruneThreshold/100)) {
+                    let seconds = Double(cumulativeTime) / 1_000_000
+                    print("\(csvEscape(element.key.program)),\(csvEscape(element.key.function)),\(seconds)")
+                }
             }
         }
     }
@@ -143,7 +195,7 @@ class SwiftProfileETLDump {
 
         let eventType = Self.getDataColumn(columns[0])
 
-        guard let timeOffset = Self.parseInt64(Self.getDataColumn(columns[1])) else {
+        guard let timeOffset = Self.getInt64Column(columns[1]) else {
             return finalizePendingSample(&pendingSample)
         }
 
@@ -169,20 +221,36 @@ class SwiftProfileETLDump {
 
     private static func parseSampleProfile(timeOffset: Int64, columns: [Data.SubSequence]) -> ProfileSample? {
         guard columns.count > 6 else { return nil }
-        let programCol = trimSpaces(columns[2])
-        guard programCol.starts(with: swiftBytes) else {
+        let programAndPidCol = trimSpaces(columns[2])
+        guard programAndPidCol.starts(with: swiftBytes) else {
             return nil
         }
 
-        guard let program = getStringColumn(programCol),
+        guard let programAndPid = getStringColumn(programAndPidCol),
+              let openParenIndex = programAndPid.lastIndex(of: "("),
+              let closeParenIndex = programAndPid.lastIndex(of: ")"),
+              openParenIndex < closeParenIndex else {
+            return nil
+        }
+
+        let program = programAndPid[..<openParenIndex].trimmingCharacters(in: .whitespaces)
+        let processIdString = programAndPid[programAndPid.index(after: openParenIndex)..<closeParenIndex]
+            .trimmingCharacters(in: .whitespaces)
+        guard let processId = Int(processIdString) else {
+            return nil
+        }
+
+        guard let threadId = getIntColumn(columns[3]),
               let firstFrame = getStringColumn(columns[6]),
-              let numSamples = parseInt(columns[columns.count - 2]) else {
+              let numSamples = getIntColumn(columns[columns.count - 2]) else {
             return nil
         }
 
         return ProfileSample(
             timeOffset: timeOffset,
             program: program,
+            processId: processId,
+            threadId: threadId,
             stack: [firstFrame],
             numSamples: numSamples
         )
@@ -201,21 +269,19 @@ class SwiftProfileETLDump {
         return pendingSample
     }
 
-    private static func parseInt64(_ slice: Data.SubSequence) -> Int64? {
-        guard let stringValue = getStringColumn(slice) else { return nil }
-        return Int64(stringValue)
-    }
-
-    private static func parseInt(_ slice: Data.SubSequence) -> Int? {
-        guard let stringValue = getStringColumn(slice) else { return nil }
-        return Int(stringValue)
-    }
-
     private static func getDataColumn(_ data: Data.SubSequence) -> Data {
         Data(trimSpaces(data))
     }
 
     private static func getStringColumn(_ data: Data.SubSequence) -> String? {
         String(data: getDataColumn(data), encoding: .utf8)
+    }
+
+    private static func getInt64Column(_ slice: Data.SubSequence) -> Int64? {
+        getStringColumn(slice).flatMap { Int64($0) }
+    }
+
+    private static func getIntColumn(_ slice: Data.SubSequence) -> Int? {
+        getStringColumn(slice).flatMap { Int($0) }
     }
 }
