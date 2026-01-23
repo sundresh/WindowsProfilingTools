c69a4326a5c1c5da229737fc6a6f783bba3c053b
Sameer Sundresh <sameer@sundresh.org>
2026-01-22 17:33:43 -0700

    Simplify analyze_swiftc_trace_csv.py to use Frontend.WallClockMicroseconds

--- analyze_swiftc_trace_csv.py
+++ analyze_swiftc_trace_csv.py
@@ -8,43 +8,43 @@
 #
 import collections
 import csv
+import glob
+import os
+import sys
+import typing
 
-#csv_reader = csv.reader(open("../13/stats/trace-1768346843829391-swift-frontend-Example-Example.swift-arm64_apple_macosx15.0-o-Onone-2390402430.csv"))
-csv_reader = csv.reader(open("stats/trace-1768419180674345-swift-frontend-hello-hello.swift-arm64_apple_macosx15.0-o-Onone-195120723.csv"))
+STATS_DIR = "S:\\Temp\\stats"
+#STATS_DIR = "C:\\Users\\sundresh\\Downloads"
+
+trace_files = glob.glob(os.path.join(STATS_DIR, "trace-*.csv"))
+if len(trace_files) != 1:
+    sys.exit(f"Error: Expected exactly 1 trace-*.csv file in {STATS_DIR}, found {len(trace_files)}")
+
+csv_reader = csv.reader(open(trace_files[0]))
 column_headers = next(csv_reader)
 
-event_to_counter_names = collections.defaultdict(lambda: set())
-event_name_stack = []
-last_pushed_event_key = None
-last_popped_event_key = None
-max_stack_depth = 0
-longest_stack = []
-for row in csv_reader:
-  event_key = tuple(row[0:4])
-  is_entry = row[2]
-  event_name = row[3]
-  counter_name = row[4]
-  if counter_name in event_to_counter_names[event_key]:
-    print(f"Duplicate: {row}")
-  event_to_counter_names[event_key].add(counter_name)
-  if is_entry == "entry":
-    if event_key != last_pushed_event_key:
-      print(("  " * len(event_name_stack)) + repr(event_key))
-      event_name_stack.append(event_name)
-      last_pushed_event_key = event_key
-  else:
-    if event_key != last_popped_event_key:
-      assert(is_entry == "exit")
-      assert(len(event_name_stack) > 0)
-      assert(event_name_stack[-1] == event_name)
-      event_name_stack.pop()
-      last_popped_event_key = event_key
-      print(("  " * len(event_name_stack)) + repr(event_key))
-  if len(event_name_stack) > max_stack_depth:
-    max_stack_depth = len(event_name_stack)
-    longest_stack = event_name_stack[:]
+def format_rows_recursively(csv_reader, depth):
+  output_lines = []
+  for row in csv_reader:
+    is_exit = row[2] == "exit"
+    event_name = row[3]
+    counter_name = row[4]
+    counter_value = int(row[6])
+    #if counter_name in ("Frontend.NumInstructionsExecuted", "Frontend.NumCyclesExecuted"):
+    if counter_name == "Frontend.WallClockMicroseconds":
+      if is_exit:
+        return (output_lines, row)
+      returned_output_lines, exit_row = format_rows_recursively(csv_reader, depth + 1)
+      if exit_row is not None:
+        assert(exit_row[3] == event_name)
+        delta_t = (int(exit_row[6]) - counter_value)/1000000.0
+        output_lines.append("  " * depth + f"{event_name} {delta_t}")
+      else:
+        output_lines.append("  " * depth + event_name)
+      output_lines.extend(returned_output_lines)
+  return (output_lines, None)
 
-print("-------------------------------------------------------------")
-print(event_name_stack)
-print(max_stack_depth)
-print(longest_stack)
+output_lines, exit_row = format_rows_recursively(csv_reader, 0)
+assert(exit_row is None)
+for output_line in output_lines:
+  print(output_line)
