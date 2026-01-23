78ebbcab5f3d06ad3d06356d3f12206c938f43c7
Sameer Sundresh <sameer@sundresh.org>
2026-01-23 01:35:48 -0700

    Handle truncated trace CSVs and suppress printing deep nesting in analyze_swift_trace_csv.py

--- analyze_swiftc_trace_csv.py
+++ analyze_swiftc_trace_csv.py
@@ -13,19 +13,25 @@ import os
 import sys
 import typing
 
-STATS_DIR = "S:\\Temp\\stats"
+#STATS_DIR = "S:\\Temp\\stats"
 #STATS_DIR = "C:\\Users\\sundresh\\Downloads"
+STATS_DIR = "S:\\Temp\\cassowary\\stats"
 
-trace_files = glob.glob(os.path.join(STATS_DIR, "trace-*.csv"))
+MAX_PRINT_DEPTH = 2
+
+trace_files = glob.glob(os.path.join(STATS_DIR, "trace-*all-x*.csv"))
 if len(trace_files) != 1:
     sys.exit(f"Error: Expected exactly 1 trace-*.csv file in {STATS_DIR}, found {len(trace_files)}")
 
 csv_reader = csv.reader(open(trace_files[0]))
 column_headers = next(csv_reader)
+last_row = None
 
 def format_rows_recursively(csv_reader, depth):
   output_lines = []
   for row in csv_reader:
+    global last_row
+    last_row = row
     is_exit = row[2] == "exit"
     event_name = row[3]
     counter_name = row[4]
@@ -38,9 +44,12 @@ def format_rows_recursively(csv_reader, depth):
       if exit_row is not None:
         assert(exit_row[3] == event_name)
         delta_t = (int(exit_row[6]) - counter_value)/1000000.0
-        output_lines.append("  " * depth + f"{event_name} {delta_t}")
+        if depth < MAX_PRINT_DEPTH:
+          output_lines.append("  " * depth + f"{event_name} {delta_t}")
       else:
-        output_lines.append("  " * depth + event_name)
+        delta_t = (int(last_row[6]) - counter_value)/1000000.0
+        if depth < MAX_PRINT_DEPTH:
+          output_lines.append("  " * depth + f"{event_name} >={delta_t}")
       output_lines.extend(returned_output_lines)
   return (output_lines, None)
 
